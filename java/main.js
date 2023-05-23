6d24c9beebd8825f6ba1118175e66bcd5418f1e5
NaRubilar~ <na.rubilar@duocuc.cl>
2023-05-23 00:24:30 -0400

    javafinally

--- java/main.js
+++ java/main.js
@@ -12,7 +12,6 @@ function validarFormulario() {
   var apellidoMaterno = document.getElementById("apellidoMa").value;
   var rut = document.getElementById("rut").value;
   var edad = document.getElementById("edad").value;
-  var fechaNacimiento = document.getElementById("fechNac").value;
   var radios = document.getElementsByName("genero");
   var correoElectronico = document.getElementById("correo").value;
   var celular = document.getElementById("celular").value;
@@ -105,13 +104,13 @@ function validarFormulario() {
         break;
       }
     }
-  
+
     // Si no se ha seleccionado ninguna opción, mostrar alerta
     if (!seleccionado) {
       alert("Debes seleccionar una opción en genero.");
       return false;
     }
-    
+
     if (celular === "") {
         alert("Por favor, ingresa tu número de celular.");
         return ;
@@ -130,56 +129,56 @@ function validarFormulario() {
       alert("El Celular debe tener al menos 8 caracteres.");
       return;
     }
-  
+
     if (correoElectronico.indexOf("@") === -1 || correoElectronico.indexOf(".") === -1) {
         alert("El correo electrónico debe contener un símbolo de arroba (@) y un punto (.)");
         return ;
       }
-  
-  
-    // Validar contraseña 
+
+
+    // Validar contraseña
     if (contrasena.length < 8) {
         alert("La contraseña debe tener al menos 8 caracteres.");
         return ;
     }
-    
+
       // Validar presencia de una letra
       if (!/[a-zA-Z]/.test(contrasena)) {
         alert("La contraseña debe contener al menos una letra.");
         return ;
     }
-    
+
       // Validar presencia de un número
     if (!/[0-9]/.test(contrasena)) {
         alert("La contraseña debe contener al menos un número.");
         return ;
     }
-    
+
     // Validar presencia de algún símbolo
     if (!/[^a-zA-Z0-9]/.test(contrasena)) {
         alert("La contraseña debe contener al menos un símbolo.");
         return ;
     }
-  
+
     if (contrasena !== repetirContrasena) {
       alert("Las contraseñas no coinciden.");
       return ;
     }
-  
+
     // Si todos los campos son válidos, se puede enviar el formulario
     return true;
 }
-    
+
     function validarLogin() {
         var usuario = document.getElementById("loginName").value;
         var contraseña = document.getElementById("loginPassword").value;
-      
+
         // Validar que los campos no estén vacíos
         if (usuario === "" || contraseña === "") {
           alert("Por favor, ingresa el usuario y la contraseña.");
           return false;
         }
-      
+
         // Validar las credenciales
         // Aquí puedes agregar tu lógica para comparar las credenciales ingresadas con las válidas
         if (usuario === "na.rubilar@gmail.com" && contraseña === "narubilar123") {
@@ -194,27 +193,27 @@ function validarFormulario() {
 
 /*
 
-    // Extra small devices (phones, 600px and down) 
+    // Extra small devices (phones, 600px and down)
     @media only screen and (max-width: 600px) {
     img {
         width: 20%;
     }
 
-    
+
     }
 
-    // Small devices (portrait tablets and large phones, 600px and up) 
+    // Small devices (portrait tablets and large phones, 600px and up)
     @media only screen and (min-width: 600px) {
     img {
         width: 20%;
     }
     }
-    // Medium devices (landscape tablets, 768px and up) 
+    // Medium devices (landscape tablets, 768px and up)
     @media only screen and (min-width: 768px) {
     img {
         width: 90%;
     }
-    } 
+    }
 
 */
 
@@ -222,11 +221,11 @@ function validarFormulario() {
 $(window).on("resize", function() {
     ajustarImagenesResponsivas();
   });
-  
+
   $(document).ready(function() {
     ajustarImagenesResponsivas();
   });
-  
+
   function ajustarImagenesResponsivas() {
     $("img").each(function() {
       $(this).css("max-width", "100%");
@@ -234,4 +233,4 @@ $(window).on("resize", function() {
     });
   }
 */
- 
+
